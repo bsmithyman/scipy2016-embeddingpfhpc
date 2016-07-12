f7e7771504922da55ec31ec80d72e13d8914df1b
Brendan Smithyman <brendan@bitsmithy.net>
2016-07-11 21:45:47 -0400

    Embed example.

--- embed/host.c
+++ embed/host.c
@@ -1,9 +1,11 @@
 
+#include <stdio.h>
 #include <Python.h>
 
 int main(int argc, char *argv[]) {
     Py_SetProgramName(argv[0]);
     Py_Initialize();
+    printf(" C: Hello World!\n");
     PyRun_SimpleString("print ' P: Hello World!'\n");
     Py_Finalize();
     return 0;
