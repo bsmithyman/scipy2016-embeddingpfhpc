d3d9892d3e1f9929c0da6edd0dc0b290e792ba56
Brendan Smithyman <brendan@bitsmithy.net>
2016-07-11 23:56:44 -0400

    Working format strings.

--- embed/embed.f
+++ embed/embed.f
@@ -9,14 +9,14 @@
       end do
 
       write (*,*) 'Values in Fortran (before Python):'
-      write (*,*) (myarr(i), i=1,5)
+      write (*,'(5f5.1)') (myarr(i), i=1,5)
 
       call init_py()
       call register_1d_real_array('myarr', myarr, nelem)
       call run_py('myfunction()')
 
       write (*,*) 'Values in Fortran (after Python):'
-      write (*,*) (myarr(i), i=1,5)
+      write (*,'(5f5.1)') (myarr(i), i=1,5)
 
       call dest_py()
       end program
